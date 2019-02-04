8df4e016d417aac151e816594d0c9f85a09747a2
marwann <marwann@gmail.com>
2019-02-04 11:01:26 +0000

    Various responsive fixes

--- lib/middleman-gibberish.rb
+++ lib/middleman-gibberish.rb
@@ -149,6 +149,7 @@ module ::Middleman
             <head>
 
               <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
 
               <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>  
               <script type="text/javascript">WebFont.load({  google: {    families: ["Poppins:100,200,300,regular,500,600,700,800,900","Source Sans Pro:200,300,regular,600,700,900"]  }});</script>
@@ -233,6 +234,10 @@ module ::Middleman
                   margin-bottom:10px;
                 }
 
+                .gibberish-submit {
+                  margin-block-end:0px;
+                }
+
               </style>
 
               </head>
@@ -263,29 +268,36 @@ module ::Middleman
 
               <div class="container">
                 <div class="row">
-                  <div class="col-8 offset-2">
+                  <div class="col-xs-12 col-lg-6 offset-lg-3">
                     <div class="card text-center">
                       <div class='gibberish'>
                         <div class='card-header gibberish-instructions'>
                           Entrez vos identifiants
                         </div>
-                        <div class="card-body">
                         <form id="gibberish-submit" name="gibberish-submit" class="gibberish-submit">
+                        <div class="card-body">
                           <input type="email" id="email" placeholder="Votre e-mail" class="form-control">
                           <input placeholder="Votre mot de passe" id='gibberish-password' name='gibberish-password' type='password' class='gibberish-password form-control'/>
                         </div>
                           <div class="card-footer">
-                          <input type="submit" class="btn btn-primary">
-                          <div class='gibberish-message'></div>
+                              <input type="submit" class="btn btn-block btn-primary" value="Connexion">
+                              <div class='gibberish-message'></div>
+                          </div>
                         </form>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/core.js" integrity="sha256-YCbKJH6u4siPpUlk130udu/JepdKVpXjdEyzje+z1pE=" crossorigin="anonymous"></script>
-              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+                <footer class="py-5 bg-dark fixed-bottom">
+                  <div class="container">
+                    <p class="m-0 text-center text-white">Copyright &copy; <a href="https://www.drop-shipping.club" target="_blank">Dropshipping Club</a> - 2019</p>
+                  </div>
+                </footer>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.js"></script>
+
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
             </body>
 
